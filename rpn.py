5656d0bd5159651947a6c5ae15446cd8d2351dea
tchon <tchon@umich.edu>
2018-11-05 20:40:09 -0500

    First commit

---
+++ rpn.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import operator
+import readline
+from termcolor import colored
+
+
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
+
+def main():
+    while True:
+        result = calculate(input("rpn calc> "))
+        if result < 0:
+            print("Result: ", colored("$ (" + str(result) + ")", 'red'))
+        elif result > 0:
+            print("Result: ", colored("$ " + str(result), 'green'))
+        else:
+            print("Result: ", str("$ -"))
+
+if __name__ == '__main__':
+    main()
